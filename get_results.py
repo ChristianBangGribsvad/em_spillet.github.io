9179d2904ad3a87959e6814fc5d1db5e56a4a0af
Matias Bundgaard-Nielsen <56378038+mabuni1998@users.noreply.github.com>
2024-05-18 13:30:31 +0200

    API call for data implemented

---
+++ get_results.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import pickle
+import os
+cwd = os.getcwd()
+
+def process_match(match):
+    hometeam = match["homeTeam"]["name"]
+    awayteam = match["awayTeam"]["name"]
+    homescore = match["score"]["fullTime"]["home"]
+    awayscore = match["score"]["fullTime"]["away"]
+    group =  match["group"]
+    stage = match["stage"]
+    score = f"{homescore} - {awayscore}"
+    if stage == "GROUP_STAGE":
+        letter = group[-1]
+        matchid = f"Group {letter} Predictions [{hometeam} - {awayteam}]"
+    else:
+        matchid = f"{stage} Predictions [{hometeam} - {awayteam}]"
+    return matchid,score
+
+def get_results():
+    uri = 'https://api.football-data.org/v4/competitions/EC/matches'
+    headers = { 'X-Auth-Token': '242e02ff31ea497fbe4b85978fe70b81' }
+    
+    response = requests.get(uri, headers=headers)
+    
+    matches  = response.json()["matches"]
+    results = []
+    for m in matches:
+        results.append(process_match(m))
+    return results
+
+
+def save_results(filename,a):
+    with open(filename, 'wb') as handle:
+        pickle.dump(a, handle)
+
+def load_results(filename):
+    with open(filename, 'rb') as handle:
+        b = pickle.load(handle)
+    return b
+
+def get_highest_result_number():
+    n_file = 0
+    for i in range(100):
+        isf = os.path.isfile(cwd + f"/results/data_{i}.pickle")
+        if isf:
+            n_file = i
+    return n_file
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-def save_countries_to_file(countries, filename):
-    with open(filename, 'w') as file:
-        for country in countries:
-            file.write(country + '\n')
+import pandas as pd
+from get_results import *
+import pickle
+from datetime import date
+import os
+cwd = os.getcwd()
 
 if __name__ == "__main__":
-    countries = [
-        "Albania", "Austria", "Belgium", "Croatia", "Czech Republic", 
-        "Denmark", "England", "France", "Georgia", "Germany", 
-        "Hungary", "Italy", "Netherlands", "Poland", "Portugal", 
-        "Romania", "Scotland", "Serbia", "Slovakia", "Slovenia", 
-        "Spain", "Switzerland", "Turkey", "Ukraine"
-    ]
+    results = get_results()
+    date = date.today()
+    datafile = [results,date]
+    n_file = get_highest_result_number()
+    
+    prev_results = load_results(cwd + f"/results/data_{n_file}.pickle")
+    if prev_results[0] != results:
+        print("New results saved")
+        save_results(cwd + f"/results/data_{n_file+1}.pickle",datafile)
+    
+    
+    
+    predictions = pd.read_csv("EM spillet 2024.csv")
+    
+    
+    
+    
+    #predictions[results[0][0]]
+        
+    
+    #response1= requests.get(uri, headers=headers)
+    #for match in response.json()['matches']:
+    #    print(match)
+      
+    #filename = "countries.txt"
+    #save_countries_to_file(countries, filename)
+    #print(f"Countries saved to {filename}")
 
-    filename = "countries.txt"
-    save_countries_to_file(countries, filename)
-    print(f"Countries saved to {filename}")
+#curl -XGET 'https://api.football-data.org/v4/competitions/PL' -H "X-Auth-Token: 242e02ff31ea497fbe4b85978fe70b81"
